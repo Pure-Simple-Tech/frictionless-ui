5ceafe0a3186276398850ada0e2194a6b7b1ff74
Valentin Harapu <aharapu2@yahoo.com>
2025-05-24 13:10:54 +0300

    chore(setup): Add webpack missing packages for storybook to start

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,7 +10,7 @@ function getAbsolutePath(value) {
 
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-webpack5-compiler-swc"),
     {
