794c154d9686de069e7ad98f86423cb391f6a04f
Valentin Harapu <aharapu2@yahoo.com>
2025-05-24 14:25:30 +0300

    chore(config): add prettier config file

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: "es5",
+  printWidth: 120,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+};
+
+export default config;
